9e2827963b6c81c319efeb764830de50b79b6e25
Prashanthkv9871 <kvprashanth987@gmail.com>
2022-02-21 16:21:21 +0530

    stocks-app

---
+++ src/Stock.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Stocks from './stocks.json';
+import './Stock.css'
+
+const Stock = () => {
+  return (
+    <>
+    <h1 className='header'>STOCKS DATA</h1>
+    <div className='container'>
+        <table>
+            <thead>
+                <tr>
+                    <th style={{textAlign:"left"}}>STOCK</th>
+                    <th style={{width:"100px"}}>DATE</th>
+                    <th>HIGH</th>
+                    <th>LOW</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                {
+                    Stocks.map((stock,index)=>{
+                        return(
+                            <tr key={index}>
+                                <td style={{textAlign:"left"}}>{stock.Stock}</td>
+                                <td>{stock.date}</td>
+                                <td>{stock.high}</td>
+                                <td>{stock.low}</td>
+                            </tr>
+                        )
+                    }) 
+                }
+            </tbody>            
+        </table>        
+    </div>
+    </>
+  )
+}
+
+export default Stock;
